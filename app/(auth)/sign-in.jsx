a2e59098dfbbce085d6036f34ab273702d44b490
minthukhant78 <minthukhant2001@outlook.com>
2024-08-17 14:36:49 +0630

    ss

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,12 +1,53 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { useState } from "react";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
 
-const SignIn = () => {
+import { images,FormField } from "../../constants";
+
+const Signin = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  })
+
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View
+          className="w-full flex justify-center h-full px-4 my-6"
 
-export default SignIn
+        >
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[34px]"
+          />
+
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Signin;
--- app/index.jsx
+++ app/index.jsx
@@ -9,66 +9,52 @@ import CustomButton from "../components/CustomButton";
 // import { useGlobalContext } from "../context/GlobalProvider";
 
 const Welcome = () => {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[298px]"
+            resizeMode="contain"
+          />
 
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with <Text className="text-secondary-200">MIK</Text>
+            </Text>
 
-    return (
-        <SafeAreaView className="bg-primary h-full">
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
 
-            <ScrollView
-                contentContainerStyle={{
-                    height: "100%",
-                }}
-            >
-                <View className="w-full flex justify-center items-center h-full px-4">
-                    <Image
-                        source={images.logo}
-                        className="w-[130px] h-[84px]"
-                        resizeMode="contain"
-                    />
-                    <Image
-                        source={images.cards}
-                        className="max-w-[380px] w-full h-[298px]"
-                        resizeMode="contain"
-                    />
-
-
-                    <View className="relative mt-5">
-                        <Text className="text-3xl text-white font-bold text-center">
-                            Discover Endless{"\n"}
-                            Possibilities with{" "}
-                            <Text className="text-secondary-200">MIK</Text>
-                        </Text>
-
-                        <Image
-                            source={images.path}
-                            className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
-                            resizeMode="contain"
-                        />
-                    </View>
-
-                    <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
             Where Creativity Meets Innovation: Embark on a Journey of Limitless
             Exploration with MTK
           </Text>
           <CustomButton
-            title ="Comtinue with Email"
-            handlePress = {() => router.push('/sign-in')}
-
+            title="Comtinue with Email"
+            handlePress={() => router.push("/sign-in")}
             contentContainerStyle="w-full mt-7"
           />
-
-
-
-
-
-
-
-                </View>
-            </ScrollView>
-                <StatusBar  backgroundColor="#161622" style="light"/>
-
-        </SafeAreaView>
-    );
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
 };
 
 export default Welcome;
